5149fa6779ee71d03b44be0f408b337d4439b977
AhmedowM <mahmedow28@gmail.com>
2026-01-10 23:31:56 +0500

    Add styling

--- tui.py
+++ tui.py
@@ -1,15 +1,18 @@
 from textual.app import App
 from textual.events import Key
-from textual.widgets import Label
+from textual.widgets import Label, Static
+from textual.containers import Container, Horizontal
 from engine import TypingEngine
 from rich.text import Text
 import time
 
 class TestApp(App):
     FILENAME = "tests.txt"
+    CSS_PATH = "style.tcss"
+    TIMEOUT = 15
 
-    def _format_string_snapshot(self, correct: bool) -> Text:
-        completed, current, remaining = self.Engine.get_string_snapshot(max_size=20)
+    def _format_string_snapshot(self, correct: bool, max_size: int = 20) -> Text:
+        completed, current, remaining = self.Engine.get_string_snapshot(max_size)
         text = Text()
         if completed:
             text.append(completed)
@@ -25,14 +28,30 @@ class TestApp(App):
         self.filename = filename or self.FILENAME
         self.Engine = TypingEngine()
         self.Engine.set_string(self.filename)
+        self.Engine.set_timeout(self.TIMEOUT)
 
     async def on_mount(self):
         self.timer = self.set_interval(0.1, self.update_ui)
 
     def compose(self):
         initial = self._format_string_snapshot(correct=True)
-        yield Label(initial,id="label")
-        yield Label("",id="stats")
+        yield Container(
+            Static(id="spacer1"),
+            Horizontal(
+                Static(id="l_spacer1"),
+                Label(initial,id="label"),
+                Static(id="l_spacer2")
+            ),
+            Static(id="spacer2"),
+            Horizontal(
+                Label("Elapsed", id="elapsed"),
+                Label("Accuracy", id="accuracy"),
+                Label("Speed", id="speed"),
+                Label("Raw Speed", id="raw_speed"),
+                id="stats_container"
+            ),
+            id="main_container"
+        )
     
     def on_key(self, event: Key):
         key_to_process = event.character
@@ -51,23 +70,30 @@ class TestApp(App):
             correct = self.Engine.is_correct()
         stats = self.Engine.get_stats(real_time=True, timestamp=timestamp)
         label1 = self.query_one("#label",Label)
-        label2 = self.query_one("#stats",Label)
-        label1.content = self._format_string_snapshot(correct)
+        label1.content = self._format_string_snapshot(correct, label1.size.width-6)
 
         if stats is not None:
-            # correct_chars = stats.correct_chars
-            # total_chars = stats.total_chars
             elapsed = max(stats.elapsed or 0, 0)
             accuracy = (stats.accuracy or 0) * 100
             speed_cpm = stats.speed or 0
             speed_wpm = speed_cpm / 5
             raw_speed_cpm = stats.raw_speed or 0
             raw_speed_wpm = raw_speed_cpm / 5
-            format = f'\nElapsed: {elapsed:.1f}s'
-            format += f'\nAccuracy: {accuracy:.0f}%'
-            format += f'\nSpeed: {speed_wpm:.2f}WPM'
-            format += f'\nRaw Speed: {raw_speed_wpm:.2f}WPM'
-            label2.content = format
+
+            elapsed_label = self.query_one("#elapsed",Label)
+            accuracy_label = self.query_one("#accuracy",Label)
+            speed_label = self.query_one("#speed",Label)
+            raw_speed_label = self.query_one("#raw_speed",Label)
+
+            elapsed_label.content = f'{elapsed:.1f}s'
+            accuracy_label.content = f'{accuracy:.0f}%'
+            speed_label.content = f'{speed_wpm:.2f}WPM'
+            raw_speed_label.content = f'{raw_speed_wpm:.2f}WPM'
+            
+            elapsed_label.border_subtitle = f'Elapsed Time'
+            accuracy_label.border_subtitle = f'Accuracy'
+            speed_label.border_subtitle = f'Speed'
+            raw_speed_label.border_subtitle = f'Raw Speed'
 
 app = TestApp()
 app.run()
